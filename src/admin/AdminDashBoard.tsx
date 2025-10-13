833a8119c7401b5af93b2a9c51bd87f66bd04193
Lucas Fernandes <lucasfernandesss23@gmail.com>
2025-10-13 16:21:04 -0300

    fix(adminDashBoard): partially fix dashboard routing

--- src/admin/AdminDashBoard.tsx
+++ src/admin/AdminDashBoard.tsx
@@ -4,35 +4,64 @@ import { VictoryPie, VictoryLabel, VictoryTheme } from "victory";
 
 const apiUrl = import.meta.env.VITE_API_URL
 
-type graficoBoardType = {
+type graficoBoardsUsuarioType = {
+  nome: string
+  boards: number
+}
+
+type graficoUsuarioComentarioType = {
+  nome: string
+  comentarios: number
+}
+
+type graficoBoardsMotivoType = {
   motivo: string
   num: number
 }
 
-type graficoUsuarioType = {
-  boards: string
-  usuario: string
-  num: number
-}
-
 type geralDadosType = {
-  usuarios: number
   boards: number
-  propostas: number
+  listas: number
+  tasks: number
+  comentarios: number
+  usuarios: number
 }
 
 export default function AdminDashboard() {
-  const [boardMotivo, setBoardMotivo] = useState<graficoBoardType[]>([])
-  const [usuario, setUsuario] = useState<graficoUsuarioType[]>([])
+  const [boardUsuario, setBoardUsuario] = useState<graficoBoardsUsuarioType[]>([])
+  const [comentarioUsuario, setComentarioUsuario] = useState<graficoUsuarioComentarioType[]>([])
+  const [boardMotivo, setBoardMotivo] = useState<graficoBoardsMotivoType[]>([])
   const [dados, setDados] = useState<geralDadosType>({} as geralDadosType)
 
   useEffect(() => {
     async function getDadosGerais() {
-      const response = await fetch(`${apiUrl}/dashboard/boardsNome`)
+      const response = await fetch(`${apiUrl}/dashboard/gerais`)
       const dados = await response.json()
       setDados(dados)
     }
     getDadosGerais()
+
+    async function getDadosBoardsUsuario() {
+      const response = await fetch(`${apiUrl}/dashboard/boardsUsuario`)
+      const dados = await response.json()
+      setBoardUsuario(dados)
+    }
+    getDadosBoardsUsuario()
+
+    async function getDadosBoardMotivo() {
+      const response = await fetch(`${apiUrl}/dashboard/boardsMotivo`)
+      const dados = await response.json()
+      setBoardMotivo(dados)
+    }
+    getDadosBoardMotivo()
+
+    async function getDadosComentarioUsuario() {
+      const response = await fetch(`${apiUrl}/dashboard/comentariosUsuario`)
+      const dados = await response.json()
+      setComentarioUsuario(dados)
+    }
+    getDadosComentarioUsuario()
+
 
   }, [])
 
@@ -40,8 +69,12 @@ export default function AdminDashboard() {
     { x: item.motivo, y: item.num }
   ))
 
-  const listaUsuario = usuario.map(item => (
-    { x: item.boards, y: item.usuario, z: item.num }
+  const listaBoardUsuario = boardUsuario.map(item => (
+    { x: item.nome, y: item.boards}
+  ))
+
+  const listaComentarioUsuario = comentarioUsuario.map(item => (
+    { x: item.nome, y: item.comentarios}
   ))
 
   return (
@@ -51,12 +84,12 @@ export default function AdminDashboard() {
       <div className="w-2/3 flex justify-between mx-auto mb-5">
         <div className="border-blue-600 border rounded p-6 w-1/3 me-3">
           <span className="bg-blue-100 text-blue-800 text-xl text-center font-bold mx-auto block px-2.5 py-5 rounded dark:bg-blue-900 dark:text-blue-300">
-            {dados.usuarios}</span>
-          <p className="font-bold mt-2 text-center">Nº Usuarios</p>
+            {dados.boards}</span>
+          <p className="font-bold mt-2 text-center">Nº Boards por Motivo</p>
         </div>
         <div className="border-red-600 border rounded p-6 w-1/3 me-3">
           <span className="bg-red-100 text-red-800 text-xl text-center font-bold mx-auto block px-2.5 py-5 rounded dark:bg-red-900 dark:text-red-300">
-            {dados.boards}</span>
+            {dados.comentarios}</span>
           <p className="font-bold mt-2 text-center">Nº Boards</p>
         </div>
         
@@ -75,7 +108,7 @@ export default function AdminDashboard() {
             style={{
               labels: {
                 fontSize: 10,
-                fill: "#fff",
+                fill: "#f0f0",
                 fontFamily: "Arial",
                 fontWeight: "bold"
               }
@@ -91,7 +124,39 @@ export default function AdminDashboard() {
             }}
             x={200}
             y={200}
-            text={["Veículos", "por Marca"]}
+            text={["Boards", "por Motivo"]}
+          />
+        </svg>
+      
+        <svg viewBox="30 55 400 400">
+          <VictoryPie
+            standalone={false}
+            width={400}
+            height={400}
+            data={listaComentarioUsuario}
+            innerRadius={50}
+            labelRadius={80}
+            theme={VictoryTheme.clean}
+            style={{
+              labels: {
+                fontSize: 10,
+                fill: "#f0f0",
+                fontFamily: "Arial",
+                fontWeight: "bold"
+              }
+            }}
+          />
+          <VictoryLabel
+            textAnchor="middle"
+            style={{
+              fontSize: 12,
+              fill: "#f00",
+              fontFamily: "Arial",
+              fontWeight: "bold"
+            }}
+            x={200}
+            y={200}
+            text={["Comentários", "por Marca"]}
           />
         </svg>
 
@@ -100,7 +165,7 @@ export default function AdminDashboard() {
             standalone={false}
             width={400}
             height={400}
-            data={listaUsuario}
+            data={listaBoardUsuario}
             innerRadius={50}
             labelRadius={80}
             theme={VictoryTheme.clean}
@@ -123,7 +188,7 @@ export default function AdminDashboard() {
             }}
             x={200}
             y={200}
-            text={["Clientes", "por Cidade"]}
+            text={["Boards", "por Usuario"]}
           />
         </svg>
 
