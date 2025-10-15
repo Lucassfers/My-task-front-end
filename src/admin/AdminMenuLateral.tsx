f588315cbad22a002ff051ece56fbeda51ed22fe
Lucas Fernandes <lucasfernandesss23@gmail.com>
2025-10-15 17:19:03 -0300

    fix(adminDashBoards): fixed graphic routes for admin

--- src/admin/AdminMenuLateral.tsx
+++ src/admin/AdminMenuLateral.tsx
@@ -19,31 +19,10 @@ export function MenuLateralAdmin() {
       <div className="h-full px-3 py-4 overflow-y-auto bg-white dark:bg-gray-800">
         <ul className="space-y-2 font-medium">
           <li>
-            <Link to="/admin/gerais" className="flex items-center p-2">
+            <Link to="/admin" className="flex items-center p-2">
               <span className="h-5 text-gray-600 text-2xl">
               </span>
-              <span className="ms-2 mt-1">Geral</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin/boardsUsuario" className="flex items-center p-2">
-              <span className="h-5 text-gray-600 text-2xl">
-              </span>
-              <span className="ms-2 mt-1">Boards Usuario</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin/comentariosUsuario" className="flex items-center p-2">
-              <span className="h-5 text-gray-600 text-2xl">
-              </span>
-              <span className="ms-2 mt-1">Comentários Usuario</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin/boardsMotivo" className="flex items-center p-2 cursor-pointer">
-              <span className="h-5 text-gray-600 text-2xl">
-              </span>
-              <span className="ms-2 mt-1">Boards Motivo</span>
+              <span className="ms-2 mt-1">Estatísticas Gerais</span>
             </Link>
           </li>
           <li>
